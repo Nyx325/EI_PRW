961b785d71c73a78b9d472db99d0af9ca24b8a87
Omar Roman <omar.roman.2828@gmail.com>
2024-12-09 18:01:13 -0600

    login medianamente funcional

--- static/js/scripts/login.js
+++ static/js/scripts/login.js
@@ -1,33 +1,35 @@
 import Alert from "../components/alert.js";
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const alert = new Alert("alert");
-    const usrI = document.getElementById("usr-input");
-    const pwdI = document.getElementById("pwd-input");
-    const form = document.getElementById("login-form");
+document.addEventListener("DOMContentLoaded", () => {
+  const alert = new Alert("alert");
+  const usrI = document.getElementById("usr-input");
+  const pwdI = document.getElementById("pwd-input");
+  const form = document.getElementById("login-form");
 
-    form.addEventListener("submit", async (e)=>{
-        e.preventDefault();
-        
-        const usr = usrI.value.trim();
-        const pwd = pwdI.value.trim();
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-        const url = "http://localhost:3000/src/routes/AuthRoutes.php";
+    const usr = usrI.value.trim();
+    const pwd = pwdI.value.trim();
 
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({usr, pwd})
-        });
+    const { hostname, port } = document.location;
+    const url = `http://${hostname}:${port}/src/routes/AuthRoutes.php`;
 
-        console.log("A");
-        if(response.status>=400){
-            alert.setMessage(await response.json());
-            alert.setVisible(true);
-            return;
-        }
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ usr, pwd }),
+    });
 
-    })
-})
+    if (response.status >= 400) {
+      const res = await response.json();
+      alert.setMessage(res.message);
+      alert.setVisible(true);
+      return;
+    }
+
+      alert.setVisible(false);
+  });
+});
